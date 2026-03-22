11362b5ff25f102fca4e172620d34938c267092c
anmolss-tech <anmolss.tech@gmail.com>
2026-03-22 13:54:28 -0700

    Create firebase-config.js

---
+++ projects/rentique/rentique-ap/services/business-side/firebase-config.js
@@ -0,0 +1,33 @@
+// @Made By: Anmol Singh
+export  const firebaseConfig = {
+  apiKey: "AIzaSyCJoXuy43sAX7lVTn6iPhw2X_DwBx8__qY",
+  authDomain: "rentique-teama.firebaseapp.com",
+  projectId: "rentique-teama",
+  storageBucket: "rentique-teama.firebasestorage.app",
+  messagingSenderId: "271728462837",
+  appId: "1:271728462837:web:6a325f1b8319e80fc12ebc"
+};
+// Main Firebase DB
+
+
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyANM-pMMswKByavxpQdkBU1kBDxY1I-JMw",
+//     authDomain: "test1-rentique.firebaseapp.com",
+//     projectId: "test1-rentique",
+//     storageBucket: "test1-rentique.firebasestorage.app",
+//     messagingSenderId: "533686296695",
+//     appId: "1:533686296695:web:f55da556f9f610c3ffe672"
+// };
+//Test1
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDnrmDxuxuRXSfH6AFr1gE1msbjFVXaK4c",
+//   authDomain: "test3-rentique.firebaseapp.com",
+//   projectId: "test3-rentique",
+//   storageBucket: "test3-rentique.firebasestorage.app",
+//   messagingSenderId: "1006663477665",
+//   appId: "1:1006663477665:web:9f1e4d2f46c3744bc443e0"
+// };
+//Test3
+
+export const GOOGLE_MAPS_API_KEY = "AIzaSyCHibjVaPQ39daz7ZZMi5cbVWIsMMZtYAY";
